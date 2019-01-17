68b5407f1c9f986d451b2b4c72c3319f862a95b3
Patrick Bryant <patrickbryant@me.com>
2019-01-17 12:36:27 -0600

    do electron scale factor

--- HistogramMgr.h
+++ HistogramMgr.h
@@ -12,160 +12,6 @@ std::string YaxisTitle(
     const std::string& unit,
     const int nbins, const double xmin, const double xmax  );
 
-class EventHisto {
-public:
-  EventHisto( const std::string& cutlevel, const bool );
-  ~EventHisto();
-
-  // Selected Tau, Muon and Electron index (leave -1 is not of this channel)
-  void Fill(
-      const int tau,
-      const int iLep,
-      const double evtweight );
-
-  void Write();
-
-private:
-  const std::string cutlevel;
-  const bool doMuon ;
-  std::map<std::string, TH1D> histmap;
-
-  TH1D& Hist( const std::string&, const bool, const bool );
-
-  void defineHist(
-    const std::string& xname,
-    const std::string& xtitle,
-    const std::string& xunit,
-    const unsigned     nbins,
-    const double       xmin,
-    const double       xmax
-  );
-
-  std::string histname(
-    const std::string& name,
-    const bool tauiso,
-    const bool os
-    );
-};
-
-/** Begin Editing */
-
-EventHisto::EventHisto( const std::string& x, const bool y ):
-  cutlevel(x), doMuon(y)
-{
-  defineHist("visMass", "visible mass", "GeV", 30 , 0 , 300  );
-  defineHist("visPt",   "visible p_{T}", "GeV", 30 , 0 , 300  );
-  defineHist("visEta",  "visible #eta", "",  24 , -2.4 , 2.4  );
-
-  defineHist("LepPt",    "Lepton p_{T}", "GeV", 20, 0 , 200 );
-  defineHist("LepEta",   "Lepton #eta", "", 24, -2.4 , 2.4 );
-  defineHist("TauPt",    "#tau p_{T}", "GeV", 20, 0 , 200 );
-  defineHist("TauEta",   "#tau p_{T}", "GeV", 20, 0 , 200 );
-  defineHist("MET",      "MET", "GeV", 20, 0 , 200 );
-  defineHist("LepTMass", "Lepton Transverse Mass", "GeV", 20, 0, 50 );
-  defineHist("DeltaRLep", "#Delta R(#tau,l)", "", 62,0,6.2);
-  defineHist("DeltaRJet", "#Delta R_{min}(#tau,jet)", "",62,0,6.2);
-  defineHist("DeltaPhiTMET","#Delta #phi(#tau,MET)", "", 62,-3.2,3.2);
-  defineHist("DeltaPhiLepMET", "#Delta #phi(l,MET)", "", 62,-3.2,3.2);
-}
-
-void EventHisto::Fill( const int tau, const int iLep, const double evtweight )
-{
-  static const double muMass = 0.10565837;
-  static const double elMass = 0.000511  ;
-
-  TLorentzVector tauP4, lepP4, METP4;
-  tauP4.SetPtEtaPhiM( tauPt->at(tau), tauEta->at(tau), tauPhi->at(tau), tauMass->at(tau) );
-  if( doMuon )
-    lepP4.SetPtEtaPhiM( muPt->at(iLep), muEta->at(iLep), muPhi->at(iLep), muMass );
-  else
-    lepP4.SetPtEtaPhiM( elePt->at(iLep), eleEta->at(iLep), elePhi->at(iLep), elMass );
-  const TLorentzVector zP4 = tauP4 + lepP4;
-  METP4.SetPtEtaPhiE(pfMET,0,pfMETPhi,pfMET);
-
-  const bool tauiso = tauByTightIsolationMVArun2v1DBoldDMwLT ->at(tau) > 0.5 ;
-  const bool os =
-    doMuon ?  ( muCharge->at(iLep) * tauCharge->at(tau)  < 0 ) :
-              ( eleCharge->at(iLep) * tauCharge->at(tau)  < 0 ) ;
-
-  const double tmass = TMass_F( lepP4.Pt(), lepP4.Px(), lepP4.Py(), pfMET, pfMETPhi );
-
-  // Begin filling
-  Hist("visMass",tauiso,os).Fill( zP4.M(), evtweight );
-  Hist("visPt"  ,tauiso,os).Fill( zP4.Pt(), evtweight );
-  Hist("visEta" ,tauiso,os).Fill( zP4.Eta(), evtweight );
-
-  Hist("LepPt", tauiso, os).Fill( lepP4.Pt(),evtweight );
-  Hist("LepEta", tauiso, os).Fill( lepP4.Eta(), evtweight );
-  Hist("TauPt",tauiso, os).Fill( tauP4.Pt(), evtweight );
-  Hist("TauEta", tauiso, os).Fill( tauP4.Pt(), evtweight );
-  Hist("LepTMass", tauiso, os).Fill( tmass, evtweight );
-
-  // angle variables
-  Hist("DeltaRLep",tauiso,os).Fill( tauP4.DeltaR(lepP4), evtweight );
-  Hist("DeltaPhiTMET",tauiso,os).Fill( tauP4.DeltaPhi(METP4), evtweight );
-  Hist("DeltaPhiLepMET",tauiso,os).Fill( lepP4.DeltaPhi(METP4), evtweight );
-
-  double mindeltaR = 1000;
-  for( int i = 0 ; i < nJet ; ++i ){
-    TLorentzVector jetp4;
-    jetp4.SetPtEtaPhiE( jetPt->at(i), jetEta->at(i), jetPhi->at(i), jetEn->at(i));
-    mindeltaR = std::min( jetp4.DeltaR( tauP4 ), mindeltaR );
-  }
-  if( mindeltaR < 1000 ) {
-    Hist("DeltaRJet", tauiso, os).Fill(mindeltaR, evtweight );
-  }
-}
-
-/** Don't Edit After this! */
-
-void EventHisto::defineHist(
-    const std::string& xname,
-    const std::string& xtitle,
-    const std::string& xunit,
-    const unsigned     nbins,
-    const double       xmin,
-    const double       xmax )
-
-{
-  for( const auto x : {true,false}){
-    for( const auto y : {true,false} ){
-      const std::string name = histname(xname,x,y);
-      histmap[name] = TH1D( name.c_str(), name.c_str(), nbins, xmin, xmax );
-      histmap[name].GetXaxis()->SetTitle( XaxisTitle(xtitle,xunit).c_str() );
-      histmap[name].GetYaxis()->SetTitle( YaxisTitle(xunit,nbins,xmin,xmax).c_str() );
-    }
-  }
-}
-
-std::string EventHisto::histname(
-  const std::string& x,
-  const bool tauiso,
-  const bool os
-)
-{
-  const std::string tauisostr = tauiso ? "Iso" : "antiIso" ;
-  const std::string osstr     = os ? "OS" : "SS";
-  return cutlevel + "_" + x + "_" + tauisostr + "_" + osstr ;
-}
-
-TH1D& EventHisto::Hist(
-  const std::string& x , const bool tauiso, const bool os )
-{
-  return histmap.at( histname(x,tauiso,os) );
-}
-
-
-void EventHisto::Write()
-{
-  for( const auto& hist : histmap ){
-    hist.second.Write();
-  }
-}
-
-EventHisto::~EventHisto() {}
-
-
 ///////////////////////////////////////////////////////////////////
 
 class ObjectHisto{
@@ -174,6 +20,7 @@ public:
   ~ObjectHisto();
 
   void Fill( const double eventweight, const int tau = -1 , const int lep=-1 );
+  void Fill( const double eventweight, const int tau , TLorentzVector tauP4 , const int lep=-1 );
   void Write();
 private:
   const std::string cutlevel;
@@ -197,7 +44,7 @@ private:
 /*** Begin Histogram definition */
 
 ObjectHisto::ObjectHisto( const std::string& cutLevel, const bool domuon ):
-  cutlevel( cutLevel ), doMuon(doMuon)
+ cutlevel( cutLevel ), doMuon(domuon)
 {
   defineHist("nJets", "Num. of Jets", "", 10, 0,10);
   defineHist("JetPt", "Jet p_{T}",          "GeV", 50,   0, 500 );
@@ -214,6 +61,51 @@ ObjectHisto::ObjectHisto( const std::string& cutLevel, const bool domuon ):
 
   defineHist("MET","Missing Transverse Energy", "GeV", 30,0,300);
   defineHist("TMass", "Lepton transverse mass", "GeV", 50,0,150);
+}
+
+void ObjectHisto::Fill( const double eventweight, const int tau, TLorentzVector tauP4, const int iLep )
+{
+  // Setting up variables
+  static const double muMass = 0.10565837;
+  static const double elMass = 0.000511  ;
+  TLorentzVector lepP4;
+  //if( tau >= 0 )
+  //  tauP4.SetPtEtaPhiM( tauPt->at(tau), tauEta->at(tau), tauPhi->at(tau), tauMass->at(tau) );
+  if( doMuon && iLep >= 0 )
+    lepP4.SetPtEtaPhiM( muPt->at(iLep), muEta->at(iLep), muPhi->at(iLep), muMass );
+  else if ( iLep >= 0 )
+    lepP4.SetPtEtaPhiM( elePt->at(iLep), eleEta->at(iLep), elePhi->at(iLep), elMass );
+  const double tmass = TMass_F( lepP4.Pt(), lepP4.Px(), lepP4.Py(), pfMET, pfMETPhi );
+
+  // Filling jet histograms
+  for( int i = 0 ; i < nJet ; ++i ){
+    Hist("JetPt").Fill( jetPt->at(i), eventweight );
+    Hist("JetEta").Fill( jetEta->at(i), eventweight );
+
+    if( i == 0 ){
+      Hist("Jet1Pt").Fill( jetPt->at(i), eventweight );
+    }
+  }
+
+  // Muon histograms
+  for( int i = 0 ; i < nMu ; ++i ){
+    Hist("MuonPt").Fill( muPt->at(i), eventweight );
+    Hist("MuonEta").Fill( muEta->at(i), eventweight );
+  }
+
+  for( int i = 0 ; i < nEle; ++i ){
+    Hist("ElecPt").Fill( elePt->at(i), eventweight );
+    Hist("ElecEta").Fill( eleEta->at(i), eventweight );
+  }
+
+  if(iLep >=0 ){
+    Hist("ElecPt").Fill( lepP4.Pt(), eventweight );
+    Hist("ElecEta").Fill( lepP4.Eta(), eventweight );
+    Hist("TMass").Fill( tmass, eventweight );
+  }
+
+  Hist("MET").Fill(pfMET);
+  // TBC
 }
 
 void ObjectHisto::Fill( const double eventweight, const int tau, const int iLep )
@@ -326,5 +218,160 @@ std::string YaxisTitle(
   }
   return ans;
 }
+
+class EventHisto {
+public:
+  EventHisto( const std::string& cutlevel, const bool );
+  ~EventHisto();
+
+  // Selected Tau, Muon and Electron index (leave -1 is not of this channel)
+  void Fill(
+      const int tau,
+      TLorentzVector tauP4,
+      const int iLep,
+      const double evtweight );
+
+  void Write();
+
+private:
+  const std::string cutlevel;
+  const bool doMuon ;
+  std::map<std::string, TH1D> histmap;
+  
+  TH1D& Hist( const std::string&, const bool, const bool );
+
+  void defineHist(
+    const std::string& xname,
+    const std::string& xtitle,
+    const std::string& xunit,
+    const unsigned     nbins,
+    const double       xmin,
+    const double       xmax
+  );
+
+  std::string histname(
+    const std::string& name,
+    const bool tauiso,
+    const bool os
+    );
+};
+
+/** Begin Editing */
+
+EventHisto::EventHisto( const std::string& x, const bool y ):
+cutlevel(x), doMuon(y)
+{
+  defineHist("visMass", "visible mass", "GeV", 30 , 0 , 300  );
+  defineHist("visPt",   "visible p_{T}", "GeV", 30 , 0 , 300  );
+  defineHist("visEta",  "visible #eta", "",  24 , -2.4 , 2.4  );
+
+  defineHist("LepPt",    "Lepton p_{T}", "GeV", 20, 0 , 200 );
+  defineHist("LepEta",   "Lepton #eta", "", 24, -2.4 , 2.4 );
+  defineHist("TauPt",    "#tau p_{T}", "GeV", 20, 0 , 200 );
+  defineHist("TauEta",   "#tau p_{T}", "GeV", 20, 0 , 200 );
+  defineHist("MET",      "MET", "GeV", 20, 0 , 200 );
+  defineHist("LepTMass", "Lepton Transverse Mass", "GeV", 20, 0, 50 );
+  defineHist("DeltaRLep", "#Delta R(#tau,l)", "", 62,0,6.2);
+  defineHist("DeltaRJet", "#Delta R_{min}(#tau,jet)", "",62,0,6.2);
+  defineHist("DeltaPhiTMET","#Delta #phi(#tau,MET)", "", 62,-3.2,3.2);
+  defineHist("DeltaPhiLepMET", "#Delta #phi(l,MET)", "", 62,-3.2,3.2);
+}
+
+void EventHisto::Fill( const int tau, TLorentzVector tauP4, const int iLep, const double evtweight )
+{
+  static const double muMass = 0.10565837;
+  static const double elMass = 0.000511  ;
+
+  TLorentzVector lepP4, METP4;
+  //tauP4.SetPtEtaPhiM( tauPt->at(tau), tauEta->at(tau), tauPhi->at(tau), tauMass->at(tau) );
+  if( doMuon )
+    lepP4.SetPtEtaPhiM( muPt->at(iLep), muEta->at(iLep), muPhi->at(iLep), muMass );
+  else
+    lepP4.SetPtEtaPhiM( elePt->at(iLep), eleEta->at(iLep), elePhi->at(iLep), elMass );
+  const TLorentzVector zP4 = tauP4 + lepP4;
+  METP4.SetPtEtaPhiE(pfMET,0,pfMETPhi,pfMET);
+
+  const bool tauiso = tauByTightIsolationMVArun2v1DBoldDMwLT ->at(tau) > 0.5 ;
+  const bool os =
+    doMuon ?  ( muCharge->at(iLep) * tauCharge->at(tau)  < 0 ) :
+              ( eleCharge->at(iLep) * tauCharge->at(tau)  < 0 ) ;
+
+  const double tmass = TMass_F( lepP4.Pt(), lepP4.Px(), lepP4.Py(), pfMET, pfMETPhi );
+
+  // Begin filling
+  Hist("visMass",tauiso,os).Fill( zP4.M(), evtweight );
+  Hist("visPt"  ,tauiso,os).Fill( zP4.Pt(), evtweight );
+  Hist("visEta" ,tauiso,os).Fill( zP4.Eta(), evtweight );
+
+  Hist("LepPt", tauiso, os).Fill( lepP4.Pt(),evtweight );
+  Hist("LepEta", tauiso, os).Fill( lepP4.Eta(), evtweight );
+  Hist("TauPt",tauiso, os).Fill( tauP4.Pt(), evtweight );
+  Hist("TauEta", tauiso, os).Fill( tauP4.Pt(), evtweight );
+  Hist("LepTMass", tauiso, os).Fill( tmass, evtweight );
+
+  // angle variables
+  Hist("DeltaRLep",tauiso,os).Fill( tauP4.DeltaR(lepP4), evtweight );
+  Hist("DeltaPhiTMET",tauiso,os).Fill( tauP4.DeltaPhi(METP4), evtweight );
+  Hist("DeltaPhiLepMET",tauiso,os).Fill( lepP4.DeltaPhi(METP4), evtweight );
+
+  double mindeltaR = 1000;
+  for( int i = 0 ; i < nJet ; ++i ){
+    TLorentzVector jetp4;
+    jetp4.SetPtEtaPhiE( jetPt->at(i), jetEta->at(i), jetPhi->at(i), jetEn->at(i));
+    mindeltaR = std::min( jetp4.DeltaR( tauP4 ), mindeltaR );
+  }
+  if( mindeltaR < 1000 ) {
+    Hist("DeltaRJet", tauiso, os).Fill(mindeltaR, evtweight );
+  }
+}
+
+/** Don't Edit After this! */
+
+void EventHisto::defineHist(
+    const std::string& xname,
+    const std::string& xtitle,
+    const std::string& xunit,
+    const unsigned     nbins,
+    const double       xmin,
+    const double       xmax )
+
+{
+  for( const auto x : {true,false}){
+    for( const auto y : {true,false} ){
+      const std::string name = histname(xname,x,y);
+      histmap[name] = TH1D( name.c_str(), name.c_str(), nbins, xmin, xmax );
+      histmap[name].GetXaxis()->SetTitle( XaxisTitle(xtitle,xunit).c_str() );
+      histmap[name].GetYaxis()->SetTitle( YaxisTitle(xunit,nbins,xmin,xmax).c_str() );
+    }
+  }
+}
+
+std::string EventHisto::histname(
+  const std::string& x,
+  const bool tauiso,
+  const bool os
+)
+{
+  const std::string tauisostr = tauiso ? "Iso" : "antiIso" ;
+  const std::string osstr     = os ? "OS" : "SS";
+  return cutlevel + "_" + x + "_" + tauisostr + "_" + osstr ;
+}
+
+TH1D& EventHisto::Hist(
+  const std::string& x , const bool tauiso, const bool os )
+{
+  return histmap.at( histname(x,tauiso,os) );
+}
+
+
+void EventHisto::Write()
+{
+  for( const auto& hist : histmap ){
+    hist.second.Write();
+  }
+}
+
+EventHisto::~EventHisto() {}
+
 
 #endif
